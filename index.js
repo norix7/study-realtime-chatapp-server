8db20431adafd421163eb90f0e8ba99a9267acc0
norix7 <norix7@gmail.com>
2022-07-13 19:40:29 +0900

    ローカルで動くように戻した…

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
     // 最後にスラッシュ/を付けるとCORSエラーになるので気をつけること
-    origin: ["https://study-realtime-chatapp-client.vercel.app", "http://localhost:3000"],
+    origin: "http://localhost:3000"
     // credentials: true
   },
   // サーバー側とクライアント側の両方で、第2引数にpathを同値で設定する必要がある。
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
 });
 
 
-const PORT = 3000;
+const PORT = 3001;
 
 server.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
